64dec4d3bf3c8f945bd79227dd259425b83bc3fe
Admin <admin@Mac-Admin.local>
2015-01-10 20:20:42 +0300

    Done.

--- Exm_Animation/Exm_Animation/ViewController.h
+++ Exm_Animation/Exm_Animation/ViewController.h
@@ -10,6 +10,11 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UISlider *mySlider;
+- (IBAction)sliderChanged:(id)sender;
 
+@property (weak, nonatomic) IBOutlet UILabel *minLabel;
+@property (weak, nonatomic) IBOutlet UILabel *maxLabel;
+@property (weak, nonatomic) IBOutlet UILabel *curLabel;
 @end
 
--- Exm_Animation/Exm_Animation/ViewController.m
+++ Exm_Animation/Exm_Animation/ViewController.m
@@ -8,31 +8,58 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    NSArray *namesArray;
+    NSMutableArray *imagesArray;
+    UIImageView *imageView;
+}
 
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
     
-    NSMutableArray *namesArray = [[NSMutableArray alloc] init];
+    /*
+    NSArray *namesArray = [NSArray arrayWithObjects:@"gs_1.png",@"gs_2.png",@"gs_3.png",@"gs_4.png",@"gs_5.png",@"gs_6.png",@"gs_7.png",@"gs_8.png",@"gs_9.png",@"gs_10.png",@"gs_11.png",@"gs_12.png",@"gs_13.png",@"gs_14.png",@"gs_15.png",@"gs_16.png", nil];
     NSMutableArray *imagesArray = [[NSMutableArray alloc] init];
-    
-    for (int i=0; i < 16; i++) {
-        [namesArray addObject:[NSString stringWithFormat:@"gs_%d.jpg",i]];
-        [imagesArray addObject:[UIImage imageNamed:[namesArray objectAtIndex:i]]];
+    for (int i=0; i<[namesArray count]; i++) {
+        [imagesArray addObject: [UIImage imageNamed:[namesArray objectAtIndex:i]]];
     }
     
-    
-    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(50, 50, 100, 150)];
-    
-    
-    
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(30, 50, 86, 193)];
+    imageView.animationImages = imagesArray;
+    imageView.animationDuration = 0.5;
     [self.view addSubview:imageView];
+    [imageView startAnimating];
     
+     */
+    /*
+    UIImageView *imageView2 = [[UIImageView alloc] initWithFrame:CGRectMake(150, 50, 86, 193)];
+    imageView2.animationImages = imagesArray;
+    imageView2.animationDuration = 3;
+    [self.view addSubview:imageView2];
+    [imageView2 startAnimating];
+    
+     */
+
+   namesArray = [NSArray arrayWithObjects:@"gs_1.png",@"gs_2.png",@"gs_3.png",@"gs_4.png",@"gs_5.png",@"gs_6.png",@"gs_7.png",@"gs_8.png",@"gs_9.png",@"gs_10.png",@"gs_11.png",@"gs_12.png",@"gs_13.png",@"gs_14.png",@"gs_15.png",@"gs_16.png", nil];
+    imagesArray = [[NSMutableArray alloc] init];
+    for (int i=0; i<[namesArray count]; i++) {
+        [imagesArray addObject: [UIImage imageNamed:[namesArray objectAtIndex:i]]];
+    }
+    
+    imageView = [[UIImageView alloc] initWithFrame:CGRectMake(30, 50, 86, 193)];
+    imageView.animationImages = imagesArray;
+    imageView.animationDuration = 0.5;
+    [self.view addSubview:imageView];
+    [imageView startAnimating];
+    
+    _minLabel.text = @"-1";
+    _maxLabel.text = @"5";
+    _curLabel.text = @"1";
 }
 
 - (void)didReceiveMemoryWarning {
@@ -40,4 +67,17 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)sliderChanged:(id)sender {
+  //  imageView.animationDuration = 3;
+    UISlider *slider = (UISlider *)sender;
+    NSInteger val = lround(slider.value);
+  //  self.myLabel.text = [NSString stringWithFormat:@"%d",val];
+
+ //   [imageView stopAnimating];
+    
+    imageView.animationDuration = val;
+    [imageView startAnimating];
+    
+    _curLabel.text = [NSString stringWithFormat:@"%ld",(long)val];
+}
 @end
